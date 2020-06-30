5cc47a96cfecf0199f47d6ad45fa7b47d36be21e
Cédric Cosson <cedric.cosson@outlook.fr>
2020-06-30 03:52:56 +0200

    qsdsqd

--- src/app/tasks/task/task.component.ts
+++ src/app/tasks/task/task.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ApiService } from '../../services/api-service'
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-task',
   templateUrl: './task.component.html',
@@ -7,9 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TaskComponent implements OnInit {
 
-  constructor() { }
+  post = null;
+  postId = null;
+  
 
-  ngOnInit(): void {
+  constructor(
+    private apiService: ApiService,
+    private route: ActivatedRoute) {
   }
 
+  async getPost(id) {
+    this.post = await this.apiService.getPost(id).toPromise()
+  }
+
+  ngOnInit(): void {
+    this.postId = this.route.snapshot.url['1'].path
+    this.getPost(this.postId);
+  }
 }
